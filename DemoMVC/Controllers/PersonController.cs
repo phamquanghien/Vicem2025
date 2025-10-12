940ff600273edce6fc99741d25edfc0fc93e065d
phamquanghien <pqh.cntt@gmail.com>
2025-10-12 17:29:10 +0700

    View_Person

---
+++ DemoMVC/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        private static List<Person> _people = new List<Person>
+        {
+            new() { Id = 1, FullName = "Nguyễn Văn A", Gender = "Nam", YearOfBirth = 1995, Address = "Hà Nội", Email = "a@gmail.com", PhoneNumber = "0123456789"},
+            new() { Id = 2, FullName = "Trần Thị B", Gender = "Nữ", YearOfBirth = 1990, Address = "TP.HCM", Email = "b@gmail.com", PhoneNumber = "0987654321"}
+        };
+        public IActionResult Index()
+        {
+            return View(_people);
+        }
+    }
+}

3fc20c9fc566af3c728ac5ee84d4a18100ab1bb1
phamquanghien <pqh.cntt@gmail.com>
2025-10-12 22:55:29 +0700

    DataAnnotations

--- DemoMVC/Models/Person.cs
+++ DemoMVC/Models/Person.cs
@@ -1,19 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DemoMVC.Models
 {
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Họ và tên là bắt buộc.")]
         // Họ và tên đầy đủ
         public string FullName { get; set; } = default!;
         // Giới tính (Nam / Nữ / Khác)
+        [Required(ErrorMessage = "Giới tính là bắt buộc.")]
         public string Gender { get; set; } = default!;
         // Năm sinh
+        [Required(ErrorMessage = "Năm sinh là bắt buộc.")]
+        [Range(1900, 2025, ErrorMessage = "Năm sinh không hợp lệ.")]
         public int YearOfBirth { get; set; }
         // Địa chỉ
-        public string Address { get; set; } = default!;
+        public string? Address { get; set; } = default!;
         // Số điện thoại
-        public string PhoneNumber { get; set; } = default!;
+        public string? PhoneNumber { get; set; } = default!;
         // Email liên hệ
+        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
         public string Email { get; set; } = default!;
     }
 }

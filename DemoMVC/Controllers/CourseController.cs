fd9a175853c5e887896ccbd407e2431fb18941f9
phamquanghien <pqh.cntt@gmail.com>
2025-10-16 20:21:11 +0700

    Delete_Course

--- DemoMVC/Controllers/CourseController.cs
+++ DemoMVC/Controllers/CourseController.cs
@@ -84,6 +84,39 @@ namespace DemoMVC.Controllers
             return View(course);
         }
 
+        // GET: Course/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         private bool CourseExists(int id)
         {
             return _context.Courses.Any(e => e.Id == id);

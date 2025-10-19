0eb68931970bee47e7ed425f0f6932ef0e2ce433
phamquanghien <pqh.cntt@gmail.com>
2025-10-19 22:43:02 +0700

    Evaluation

---
+++ DemoMVC/Models/ViewModels/EvaluationVM.cs
@@ -0,0 +1,10 @@
+namespace DemoMVC.Models.ViewModels
+{
+    public class EvaluationVM
+    {
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; } = string.Empty;
+        public string CourseName { get; set; } = string.Empty;
+        public List<EvaluationItemVM> Trainees { get; set; } = new();
+    }
+}
